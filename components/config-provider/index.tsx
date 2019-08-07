7c719d4dd00326d8978cd08bc749517ebcd754b3
Chenxiaoyao <chenxiaoyao6228@163.com>
2019-08-07 19:21:14 +0800

    feat(provider): add provider component

---
+++ components/config-provider/index.tsx
@@ -0,0 +1,39 @@
+import React, { Context, createContext } from 'react';
+
+const ConfigContext: Context<ConfigConsumerProps | null> = createContext({
+  getPrefixCls: (suffixCls: string, customizePrefixCls?: string) => {
+    if (customizePrefixCls) return customizePrefixCls;
+    return `one-${suffixCls}`;
+  },
+});
+export const ConfigConsumer = ConfigContext.Consumer;
+
+export interface ConfigConsumerProps {
+  getPrefixCls: (suffixCls: string, customizePrefixCls?: string) => string;
+}
+
+export interface ConfigProviderProps {
+  prefixCls?: string;
+}
+
+
+class ConfigProvider extends React.Component<ConfigProviderProps, any> {
+  getPrefixCls = (suffixCls: string, customizePrefixCls?: string) => {
+    const { prefixCls = 'one' } = this.props;
+    if (customizePrefixCls) return customizePrefixCls;
+    return suffixCls ? `${prefixCls}-${suffixCls}` : prefixCls;
+  }
+  renderProvider = (context: ConfigConsumerProps) => {
+    const { children } = this.props;
+    const config: ConfigConsumerProps = {
+      ...context,
+      getPrefixCls: this.getPrefixCls
+    }
+    return <ConfigContext.Provider value={config}>{children}</ConfigContext.Provider>
+  }
+  render() {
+    return (<ConfigConsumer>{this.renderProvider}</ConfigConsumer>);
+  }
+}
+
+export default ConfigProvider;
